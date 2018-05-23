ddc95229790e47b250b293ae31b9fcb6c69e52cd
pramodcapgemini <39541278+pramodcapgemini@users.noreply.github.com>
2018-05-23 13:39:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
 
-	//public static void main(String[] args) {
+	public static void main(String[] args) {
 		
 		 for(int i=1;i<=10;i++)
 		 System.out.println("Welcome to Jenkis World!!!!!"+i);
